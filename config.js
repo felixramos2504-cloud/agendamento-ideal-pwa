6a1123c8629bcb6826b1e014a79df695d660383b
felixramos2504-cloud <felixramos2504@gmail.com>
2026-09-25 02:45:45 -0300

    Change APP_SCRIPT_URL to a global variable
    Updated APP_SCRIPT_URL to be a global variable.

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrvZJMrtHSJDitYf0XIYeXQRb9knLq7D2UZKVLRbgqx6El6Z9bbkGQK2tmOIkwjb1Wmw/exec';
+// =========================================================
+// AGENDAMENTO IDEAL - CONFIGURAÇÃO DA PWA
+// =========================================================
+
+window.APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrvZJMrtHSJDitYf0XIYeXQRb9knLq7D2UZKVLRbgqx6El6Z9bbkGQK2tmOIkwjb1Wmw/exec';
