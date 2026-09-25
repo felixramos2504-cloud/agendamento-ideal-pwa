2245a98678790d109e00f594f8ee82d5a3af4b72
felixramos2504-cloud <felixramos2504@gmail.com>
2026-09-25 02:40:47 -0300

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-// =========================================================
-// AGENDAMENTO IDEAL - CONFIGURAÇÃO DA PWA
-// =========================================================
-
 const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrvZJMrtHSJDitYf0XIYeXQRb9knLq7D2UZKVLRbgqx6El6Z9bbkGQK2tmOIkwjb1Wmw/exec';
