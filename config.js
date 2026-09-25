784cc918dd0fd477e3e956e8493724bfef7e5598
felixramos2504-cloud <felixramos2504@gmail.com>
2026-09-25 02:38:37 -0300

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 // =========================================================
 // AGENDAMENTO IDEAL - CONFIGURAÇÃO DA PWA
 // =========================================================
-// Cole aqui a URL FINAL da implantação do seu Google Apps Script.
-// Exemplo:
-// const APP_SCRIPT_URL = 'https://script.google.com/macros/s/SEU_ID/exec';
-//
-// IMPORTANTE: use a URL /exec da implantação publicada, não a /dev.
-// =========================================================
+
 const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrvZJMrtHSJDitYf0XIYeXQRb9knLq7D2UZKVLRbgqx6El6Z9bbkGQK2tmOIkwjb1Wmw/exec';
